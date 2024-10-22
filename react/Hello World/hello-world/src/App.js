4a29a67f2ef0a3e04c2619c2f858e4762048f717
queenpatel <queen.patel.1219@gmail.com>
2024-10-22 13:27:25 +0530

    Assignment

---
+++ react/Hello World/hello-world/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2 className="App-Header">React Js</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div><p className="helloworld">Hello World</p></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
